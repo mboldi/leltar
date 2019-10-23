200836869cc86d83196341f4436fbfe4add5c0b9
Márta Boldizsár <marta.boldi13@gmail.com>
2019-10-23 17:00:46 +0200

    init commit

---
+++ app/src/main/java/hu/bme/aut/leltar/MainActivity.java
@@ -0,0 +1,20 @@
+package hu.bme.aut.leltar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
